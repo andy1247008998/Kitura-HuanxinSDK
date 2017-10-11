07c2489dcdbc76c41bfe260611044aa85f4a1244
ailion <1247008998@qq.com>
2017-10-12 00:16:06 +0800

    change package name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-        name: "Kitura-HuanxinSDK",
+        name: "KituraHuanxinSDK",
         dependencies: [
             .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0),
             .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 17),
